ccbe3ac11b0ec07ab6b7f9b737b2a58714bda887
Nemanja Gajic <nemanjagajic@Nemanjas-MacBook-Pro.local>
2022-02-04 16:09:24 +0100

    minor changes

--- packages/auth/src/components/Signin.js
+++ packages/auth/src/components/Signin.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
